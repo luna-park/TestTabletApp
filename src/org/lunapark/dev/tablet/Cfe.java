0fff18d141cee824871f19a0564c2027afab56fa
znak <tynuza@gmail.com>
2017-04-12 18:14:07 +0300

    Add Cfe class

---
+++ src/org/lunapark/dev/tablet/Cfe.java
@@ -0,0 +1,8 @@
+package org.lunapark.dev.tablet;
+
+/**
+ * Created by znak on 12.04.2017.
+ */
+public class Cfe {
+
+}
--- src/org/lunapark/dev/tablet/Main.java
+++ src/org/lunapark/dev/tablet/Main.java
@@ -1,7 +1,23 @@
 package org.lunapark.dev.tablet;
 
+import java.io.IOException;
+import java.io.Reader;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("tester");
+        Reader reader = new Reader() {
+            @Override
+            public int read(char[] cbuf, int off, int len) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public void close() throws IOException {
+
+            }
+        };
+
+
     }
 }
