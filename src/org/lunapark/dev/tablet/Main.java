f95a243daad78aff6af778892e4498904da3b1d1
znak <tynuza@gmail.com>
2017-04-12 17:15:25 +0300

    Clear Main

--- src/org/lunapark/dev/tablet/Main.java
+++ src/org/lunapark/dev/tablet/Main.java
@@ -1,16 +1,7 @@
 package org.lunapark.dev.tablet;
 
-import java.util.*;
-
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter a number: ");
-		double number1 = input.nextDouble();
-		System.out.print("Enter second number: ");
-		double number2 = input.nextDouble();
-		double product = number1 * number2;
-		System.out.printf("The product of both numbers is: %f", product);
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("tester");
     }
 }
